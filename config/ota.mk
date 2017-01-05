cd2ddda0ca1acd5fc12fd8d29895fadb8cc12864
LiquidDark <fisch890@gmail.com>
2017-01-05 13:50:33 +0100

    Update ota.mk

--- config/ota.mk
+++ config/ota.mk
@@ -1,3 +1,9 @@
+
+# OTA default build type
+ifeq ($(OTA_TYPE),)
+OTA_TYPE=Unofficial
+endif
+
 # Liquid version
 LIQUID_VERSION := LiquidDark-$(shell date +"%y%m%d")
 DEVICE := $(subst liquid_,,$(TARGET_PRODUCT))
@@ -5,5 +11,16 @@ DEVICE := $(subst liquid_,,$(TARGET_PRODUCT))
 # Build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.liquid.version=$(LIQUID_VERSION)
+    ro.ota.device=$(DEVICE) \
+    ro.ota.type=$(OTA_TYPE)
     
+$(shell echo -e "# OTA_configuration\n \
+ota_url=https://mirrors.c0urier.net/android/teamhorizon/N/$(OTA_TYPE)/$(DEVICE)/ota_nougat.xml\n \
+ota-device=$(DEVICE)\n \
+device_name=ro.ota.device\n \
+release_type=Stable\n \
+version_source=ro.xenonhd.version\n \
+version_delimiter=-\n \
+version_position=1\n \
+version_format=yyMMdd" > $(ANDROID_BUILD_TOP)/ota_conf)    
     
