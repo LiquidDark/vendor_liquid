c7adc1e56d5edfafdd6a1c5245926ddca86a8a00
t-rex600 <fisch890@gmail.com>
2017-03-05 14:58:48 +0100

    Add UBER property overides

--- config/common.mk
+++ config/common.mk
@@ -35,10 +35,31 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Enable root and ADB
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.dun.override=0 \
     persist.sys.root_access=3 \
-    ro.adb.secure=0 \
     ro.secure=0
+    
+# UBER property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=20 \
+    wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=150 \
+    debug.performance.tuning=1 \
+    ro.ril.power_collapse=1 \
+    persist.service.lgospd.enable=0 \
+    persist.service.pcsync.enable=0 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.substratum.verified=true \
+    ro.opa.eligible_device=true \
+    ro.adb.secure=1    
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
